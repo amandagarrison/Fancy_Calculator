d790a6fcc1fed032a8c0e98a68e3dbee4cd40325
Amanda <amanda.garrison@improving.com>
2021-07-12 10:40:10 -0500

    step 2

--- Program.cs
+++ Program.cs
@@ -6,43 +6,80 @@ namespace Fancy_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("A Consonle Calculator");
+            Console.WriteLine("A Fancy Consonle Calculator");
 
-            Console.WriteLine("Enter a number.");
-            var response1 = Console.ReadLine();
+            string response1 = "";
+            string response2 = "";
+            decimal num1 = 0;
+            decimal num2 = 0;
 
-            int num1;
-
-            bool parseSuccess = int.TryParse(response1, out num1);
-
-            if (parseSuccess)
+            do
             {
-                Console.WriteLine($"You entered: {num1}");
-
-            }
-            else
-            {
-                Console.WriteLine("This is not a number!");
                 Console.WriteLine("Enter a number.");
-            }
-            string num1AsString = Console.ReadLine();
+                response1 = Console.ReadLine();
+                if(decimal.TryParse(response1, out num1) == true)
+                {
+                    break;
+                } else
+                {
+                    Console.WriteLine($"{response1} is not a number");
+                }
+            } while(decimal.TryParse(response1, out num1) == false );
 
-
-            Console.WriteLine("Enter a second number and I will add it to the first");
-            var num2AsString = Console.ReadLine();
-
-            int num2;
-            bool parseSuccess2 = int.TryParse(num2AsString, out num2);
-
-            if (parseSuccess2)
-                Console.WriteLine($"You entered: {num2}");
-            else
-                Console.WriteLine("This is not a number!");
+            do
+            {
+                Console.WriteLine("Enter another number.");
+                response2 = Console.ReadLine();
+                if (decimal.TryParse(response2, out num2) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{response2} is not a number");
+                }
+            } while (decimal.TryParse(response2, out num2) == false);
 
             var result = Sum(num1, num2);
-            Console.WriteLine("Result: " + result);
+            Console.WriteLine($"Result: {result}.");
+            Console.ReadLine();
+
+            static int Sum(decimal num1, decimal num2)
+            {
+                return (int)(num1 + num2);
+            }
+
         }
 
-        static int Sum(int num1, int num2) => num1 + num2;
     }
 }
+
+//            bool parseSuccess = int.TryParse(response1, out int num1);
+
+            //            if (parseSuccess)
+            //            {
+
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("This is not a number!");
+            //                Console.WriteLine("Enter a number.");
+            //            }
+            //            string num1AsString = Console.ReadLine();
+
+
+            //            Console.WriteLine("Enter a second number and I will add it to the first");
+            //            var num2AsString = Console.ReadLine();
+
+            //            bool parseSuccess2 = int.TryParse(num2AsString, out int num2);
+
+            //            if (parseSuccess2)
+            //                Console.WriteLine($"You entered: {num2}");
+            //            else
+            //                Console.WriteLine("This is not a number!");
+
+            //            var result = Sum(num1, num2);
+            //            Console.WriteLine($"Results: {result}");
+
+            //            Console.WriteLine("Press enter to close the window");
+    
